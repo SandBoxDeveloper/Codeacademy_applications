75a33a89dd99a53b50246e51ae6e904dec1db9ff
Andre Hitchman <madetobethebest@hotmail.co.uk>
2013-08-14 11:50:12 +0100

    case statements to decide what actions to take based on the user types in. add, update, display and delete cases.

--- Ruby_apps/a_night_at_the_movies_app/movies_ratings_app.rb
+++ Ruby_apps/a_night_at_the_movies_app/movies_ratings_app.rb
@@ -10,3 +10,31 @@ movies = { ted: 68,
 #prompt user
 
 choice = puts "choice?"
+
+# create new hash table
+movies = Hash.new()
+# populate initial hash table; rating - values pairs
+movies = { ted: 68,
+        wreck_it_ralph: 86,
+        rango: 88,
+        star_treck: 95
+}
+
+#prompt user
+
+choice = puts "choice?"
+
+# case statements to decide what actions to take based on the user types in. add, update, display and delete cases.
+
+case choice
+when "add"
+    puts "Added"
+when "update"
+    puts "Updated"
+when "display"
+    puts "Movies!"
+when "delete"
+    puts "Deleted"
+else 
+    puts "Error!"
+end
