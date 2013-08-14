387a189f84f9d9a3a71f7f38111ff292ff6e1e3a
Andre Hitchman <madetobethebest@hotmail.co.uk>
2013-08-14 11:32:32 +0100

    hash table created and populated with initial moive rating pair values.

---
+++ Ruby_apps/a_night_at_the_movies_app/movies_ratings_app.rb
@@ -0,0 +1,12 @@
+# create new hash table
+movies = Hash.new
+# populate initial hash table; rating - values pairs
+movies = { ted : "68% review",
+        wreck-it-ralph: "86% review",
+        rango: "88% review",
+        star-treck: "95% review"
+}
+
+#prompt user
+
+choice = puts "choice?"
